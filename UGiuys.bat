97d0aa7bd0346fb8cc3f6f725ac7b6402d9c717e
crabwalkin <149435473+crabwalkin@users.noreply.github.com>
2025-07-23 15:36:28 -0500

    Update UGiuys.bat

--- UGiuys.bat
+++ UGiuys.bat
@@ -1,6 +1,6 @@
 cd %~dp0
-AMIDEWINx64.EXE /SM "To Be Filled By O.E.M."
-AMIDEWINx64.EXE /BM "To Be Filled By O.E.M."
+AMIDEWINx64.EXE /SM "%RANDOM%%RANDOM%-%RANDOM%"
+AMIDEWINx64.EXE /BM "%RANDOM%%RANDOM%-%RANDOM%"
 AMIDEWINx64.EXE /BP "%RANDOM%7-%RANDOM%-%RANDOM%%RANDOM%"
 AMIDEWINx64.EXE /SP "%RANDOM%%RANDOM%-%RANDOM%"
 AMIDEWINx64.EXE /SU auto
@@ -14,8 +14,8 @@ AMIDEWINx64.EXE /BLC %random%%random%
 AMIDEWINx64.EXE /PSN "%RANDOM%%RANDOM%8"
 AMIDEWINx64.EXE /PAT %random%%random%
 AMIDEWINx64.EXE /PPN %random%%random%
-AMIDEWINx64.EXE /CSK "Default string"
-AMIDEWINx64.EXE /CS "Default string"
+AMIDEWINx64.EXE /CSK "%RANDOM%%RANDOM%-%RANDOM%"
+AMIDEWINx64.EXE /CS "%RANDOM%%RANDOM%-%RANDOM%"
 AMIDEWINx64.EXE /CA %random%%random%
 AMIDEWINx64.EXE /IVN "American Megatrends International, LLC."
 AMIDEWINx64.EXE /BS "%RANDOM%-%RANDOM%-%RANDOM%"
@@ -24,12 +24,11 @@ AMIDEWINx64.EXE /SM "Custom Manufacturer %random%"
 AMIDEWINx64.EXE /SP "%random% %random%"
 AMIDEWINx64.EXE /SV "%random:~0,2%.%random:~0,1%"
 AMIDEWINx64.EXE /SK "SKU-%random%"
-AMIDEWINx64.EXE /ID "%random:~0,2%/%random:~0,2%/20%random:~0,2%"
+AMIDEWINx64.EXE /ID "%RANDOM%%RANDOM%-%RANDOM%"
 AMIDEWINx64.EXE /SS "%random%-%random%%random%"
 AMIDEWINx64.EXE /SF "%random%-%random%"
 AMIDEWINx64.EXE /CM "%random%-%random%"
 AMIDEWINx64.EXE /CV "%random%-%random%"
-AMIDEWINx64.EXE /CA "Asset-%random%"
 AMIDEWINx64.EXE /CSK "%random%-%random%"
 AMIDEWINx64.EXE /CMH 3 "%random%-%random%"
 AMIDEWINx64.EXE /CVH 3 "%random%-%random%"
@@ -44,14 +43,9 @@ AMIDEWINx64.EXE /OS 5 "OEM5-%random%"
 AMIDEWINx64.EXE /OS 6 "OEM6-%random%"
 AMIDEWINx64.EXE /OS 7 "OEM7-%random%"
 AMIDEWINx64.EXE /OS 8 "OEM8-%random%"
-AMIDEWINx64.EXE /SCO 1 "Config1-%random%"
-AMIDEWINx64.EXE /SCO 2 "Config2-%random%"
-AMIDEWINx64.EXE /SCO 3 "Config3-%random%"
-AMIDEWINx64.EXE /SCO 4 "Config4-%random%"
 AMIDEWINx64.EXE /CS "%random%-%random%"
 AMIDEWINx64.EXE /PAT "%random%-%random%"
 AMIDEWINx64.EXE /PSN "%random%-%random%%random%"
-AMIDEWINx64.EXE /PPN "Part-%random%%random%"
 AMIDEWINx64.EXE /SU auto
 AMIDEWINx64.EXE /BSH 2 %random%-%random%
 AMIDEWINx64.EXE /BS %random%-%random%%random%
