56d4b56a43ee4d20721e51aa33a515c579add1f0
crabwalkin <149435473+crabwalkin@users.noreply.github.com>
2025-08-20 16:02:42 -0500

    Update UGiuys.bat

--- UGiuys.bat
+++ UGiuys.bat
@@ -1,61 +1,65 @@
+@echo off
 cd %~dp0
-AMIDEWINx64.EXE /SM "%RANDOM%%RANDOM%-%RANDOM%"
-AMIDEWINx64.EXE /BM "%RANDOM%%RANDOM%-%RANDOM%"
-AMIDEWINx64.EXE /BP "%RANDOM%7-%RANDOM%-%RANDOM%%RANDOM%"
-AMIDEWINx64.EXE /SP "%RANDOM%%RANDOM%-%RANDOM%"
-AMIDEWINx64.EXE /SU auto
-AMIDEWINx64.EXE /SS "6%RANDOM%%RANDOM%4"
-AMIDEWINx64.EXE /CSK %random%%random%
-AMIDEWINx64.EXE /CM  %random%%random%
-AMIDEWINx64.EXE /SK %random%%random%
-AMIDEWINx64.EXE /SF %random%%random%
-AMIDEWINx64.EXE /BT %random%%random%
-AMIDEWINx64.EXE /BLC %random%%random%
-AMIDEWINx64.EXE /PSN "%RANDOM%%RANDOM%8"
-AMIDEWINx64.EXE /PAT %random%%random%
-AMIDEWINx64.EXE /PPN %random%%random%
-AMIDEWINx64.EXE /CSK "%RANDOM%%RANDOM%-%RANDOM%"
-AMIDEWINx64.EXE /CS "%RANDOM%%RANDOM%-%RANDOM%"
-AMIDEWINx64.EXE /CA %random%%random%
-AMIDEWINx64.EXE /IVN "American Megatrends International, LLC."
-AMIDEWINx64.EXE /BS "%RANDOM%-%RANDOM%-%RANDOM%"
-AMIDEWINx64.EXE /IVN "American Megatrends International, LLC."
-AMIDEWINx64.EXE /SM "Custom Manufacturer %random%"
-AMIDEWINx64.EXE /SP "%random% %random%"
-AMIDEWINx64.EXE /SV "%random:~0,2%.%random:~0,1%"
-AMIDEWINx64.EXE /SK "SKU-%random%"
-AMIDEWINx64.EXE /ID "%RANDOM%%RANDOM%-%RANDOM%"
-AMIDEWINx64.EXE /SS "%random%-%random%%random%"
-AMIDEWINx64.EXE /SF "%random%-%random%"
-AMIDEWINx64.EXE /CM "%random%-%random%"
-AMIDEWINx64.EXE /CV "%random%-%random%"
-AMIDEWINx64.EXE /CSK "%random%-%random%"
-AMIDEWINx64.EXE /CMH 3 "%random%-%random%"
-AMIDEWINx64.EXE /CVH 3 "%random%-%random%"
-AMIDEWINx64.EXE /CSH 3 "CSH-%random%"
-AMIDEWINx64.EXE /CAH 3 "CAH-%random%"
-AMIDEWINx64.EXE /CSKH 3 "CSKH-%random%"
-AMIDEWINx64.EXE /OS 1 "OEM1-%random%"
-AMIDEWINx64.EXE /OS 2 "OEM2-%random%"
-AMIDEWINx64.EXE /OS 3 "OEM3-%random%"
-AMIDEWINx64.EXE /OS 4 "OEM4-%random%"
-AMIDEWINx64.EXE /OS 5 "OEM5-%random%"
-AMIDEWINx64.EXE /OS 6 "OEM6-%random%"
-AMIDEWINx64.EXE /OS 7 "OEM7-%random%"
-AMIDEWINx64.EXE /OS 8 "OEM8-%random%"
-AMIDEWINx64.EXE /CS "%random%-%random%"
-AMIDEWINx64.EXE /PAT "%random%-%random%"
-AMIDEWINx64.EXE /PSN "%random%-%random%%random%"
-AMIDEWINx64.EXE /SU auto
-AMIDEWINx64.EXE /BSH 2 %random%-%random%
-AMIDEWINx64.EXE /BS %random%-%random%%random%
-AMIDEWINx64.EXE /CM "%random%-%random%"
-AMIDEWINx64.EXE /BM "%random%-%random%"
-cls
+setlocal EnableDelayedExpansion
+set HEXCHARS=0123456789ABCDEF
+:randhex
+set HEXSTR=
+for /L %%i in (1,1,%1) do (
+    set /A idx=!random! %% 16
+    set HEXSTR=!HEXSTR!!HEXCHARS:~%idx%,1!
+)
+exit /B
+call :randhex 5
+set CPU1=!HEXSTR!
+call :randhex 5
+set CPU2=!HEXSTR!
+AMIDEWINx64.EXE /CS "V%CPU1%-%CPU2%"
+call :randhex 4
+set CM1=!HEXSTR!
+call :randhex 4
+set CM2=!HEXSTR!
+AMIDEWINx64.EXE /CM "%CM1%-%CM2%"
+set /A B1=!random! %% 900 + 100
+set /A B2=!random! %% 900 + 100
+set /A B3=!random! %% 90000 + 10000
+AMIDEWINx64.EXE /BS "%B1%-%B2%-%B3%"
+call :randhex 6
+AMIDEWINx64.EXE /BLC "%HEXSTR%"
+call :randhex 5
+call :randhex 5
+AMIDEWINx64.EXE /BSH 2 "%HEXSTR%-%HEXSTR%"
+set /A M1=!random! %% 900000 + 100000
+set /A M2=!random! %% 900000000 + 100000000
+AMIDEWINx64.EXE /BM "MB-%M1%-%M2%"
+call :randhex 8
+set H1=!HEXSTR!
+call :randhex 4
+set H2=!HEXSTR!
+call :randhex 4
+set H3=!HEXSTR!
+call :randhex 4
+set H4=!HEXSTR!
+call :randhex 12
+set H5=!HEXSTR!
+AMIDEWINx64.EXE /SU "%H1%-%H2%-%H3%-%H4%-%H5%"
+call :randhex 4
+set CA1=!HEXSTR!
+call :randhex 4
+set CA2=!HEXSTR!
+AMIDEWINx64.EXE /CA "%CA1%-%CA2%"
+AMIDEWINx64.EXE /CAH 3 "CAH-%CA1%"
+AMIDEWINx64.EXE /CSH 3 "CSH-%CA2%"
+call :randhex 4
+AMIDEWINx64.EXE /CSKH 3 "CSKH-%HEXSTR%"
+call :randhex 4
+AMIDEWINx64.EXE /CMH 3 "%HEXSTR%-%CA1%"
+call :randhex 4
+AMIDEWINx64.EXE /CVH 3 "%HEXSTR%-%CA2%"
 net stop winmgmt /y
 net start winmgmt /y
-sc stop winmgmt
 timeout 1
-sc start winmgmt
-timeout 1
+
+cls
+echo Done. All IDs have been randomized and look legit!
+pause
 exit
